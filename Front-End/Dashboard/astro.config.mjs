f7a247265a45cef29b525bbfb849b63250f7e61b
Idiatou Diallo <idiatoudiallo@admins-MacBook-Air-2.local>
2024-05-01 00:41:08 -0400

    astro

--- Front-End/Dashboard/astro.config.mjs
+++ Front-End/Dashboard/astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
